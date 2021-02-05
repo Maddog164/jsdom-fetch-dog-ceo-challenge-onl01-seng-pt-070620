c4b8e2606364db4c52994cf41c7043c5f2c366f4
Maddog164 <denise@henderson3.us>
2021-02-04 22:26:12 -0500

    commit tree 23ea75110e640a4440eaef14d1bf7859c97e138e

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log('%c HI', 'color: firebrick')
+// console.log('%c HI', 'color: firebrick')
+
+document.addEventListenerr("DOMContentLoaded", function() {
+    loadImages();
+})
+
+function loadImages(){
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(imgUrl)
+        .then(res => res.json())
+        .then(results => {
+            results.message.forEach(image => addImage(image))
+        });
+  }
+
+  function addImage(dogPicUrl) {
+      let container = document.querySelector('#dog-image-container');
+      let newImageE1 = document.createElement('img');
+      newImageE1.src = dogPicUrl;
+      container.appendChild(newImageE1);
+  }
